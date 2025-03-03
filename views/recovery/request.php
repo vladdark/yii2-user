9f5e6a40a06dff4648f43d3cfa2830be7d652dd4
“Rodolfo <vladcarmona6@gmail.com>
2025-03-03 00:42:28 -0600

    updates views to use yii\bootstrap5

--- views/recovery/request.php
+++ views/recovery/request.php
@@ -14,32 +14,23 @@ use yii\widgets\ActiveForm;
 
 /**
  * @var yii\web\View $this
- * @var yii\widgets\ActiveForm $form
+ * @var \yii\bootstrap5\ActiveForm $form
  * @var dektrium\user\models\RecoveryForm $model
  */
 
 $this->title = Yii::t('user', 'Recover your password');
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="row">
-    <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
-            </div>
-            <div class="panel-body">
-                <?php $form = ActiveForm::begin([
-                    'id' => 'password-recovery-form',
-                    'enableAjaxValidation' => true,
-                    'enableClientValidation' => false,
-                ]); ?>
 
-                <?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+<h3 class="panel-title"><?= Html::encode($this->title) ?></h3>
 
-                <?= Html::submitButton(Yii::t('user', 'Continue'), ['class' => 'btn btn-primary btn-block']) ?><br>
+<?php $form = ActiveForm::begin([
+    'id' => 'password-recovery-form',
+    'enableAjaxValidation' => true,
+    'enableClientValidation' => false,
+]); ?>
 
-                <?php ActiveForm::end(); ?>
-            </div>
-        </div>
-    </div>
-</div>
+<?= $form->field($model, 'email')->textInput(['autofocus' => true]) ?>
+
+<?= Html::submitButton(Yii::t('user', 'Continue'), ['class' => 'btn btn-primary btn-block']) ?><br>
+
+<?php ActiveForm::end(); ?>
